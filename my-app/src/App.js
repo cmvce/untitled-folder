dbbc488d83cc99aa8c1f3fe8c1f89018823f8dda
Coleene <acevedocoleene@gmail.com>
2025-01-30 14:48:08 +0100

    updated

---
+++ my-app/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+// eslint-disable-next-line
+import axios from "axios";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>im fine thank you</h1>
+        <p>
+          hello<code>src/App.js</code> and save to reload.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
